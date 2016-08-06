1ac30b11af6706ab8bde5753ae4105717e500641
Jo Squire <mjsquire@digis.net>
2016-08-06 14:46:23 -0600

    add wallet

--- lunch_lady.rb
+++ lunch_lady.rb
@@ -1,21 +1,26 @@
-#make menu for main dishes
-  #can get description of food
-#menu for side dishes repeat for more side dishes how ever many they want
-#print out the order, can remove items or start over
-#with cost totals and calorie totals
-#loop it to get more orders
-
 require 'pry'
 @plates = []
 
+def wallet
+  puts "How much money do you have to spend?"
+  #can I change the prompt to be a dollar sign?
+  @wallet = gets.to_i
+  main_menu
+end
+
 def main_menu
   puts "What main dish would like?"
-  puts "  1: Meatloaf"
-  puts "  2: Mystery Meat"
-  puts "  3: Slop"
+  puts "  1: Meatloaf $5.00"
+  puts "  2: Mystery Meat $3.00"
+  puts "  3: Hash $2.00"
+  puts "  4: Exit"
   @main_input = gets.to_i - 1
-  @plates << @main[@main_input]
-  first_choice
+    if @main_input == 3
+      exit
+    else
+      @plates << @main[@main_input]
+      first_choice
+    end
 end
 
 @main = [{ meal: 'Meatloaf', cost: 5.00, calories: 650, description: 'Ground Beef with stuff mixed into it'},
@@ -29,17 +34,17 @@ end
 
 def sides_menu
   puts "\nWhat side dish would you like?"
-  puts "  1: Carrots"
-  puts "  2: Watermelon"
-  puts "  3: Jello"
+  puts "  1: Carrots $ .50"
+  puts "  2: Watermelon $ .75"
+  puts "  3: Jello $5.00"
   @sides_input = gets.to_i - 1
 
-#FIX THIS - for bad entry go back and try again
-  #  if @sides_input == 0
-  #    puts "Invalid entry, please select an item."
-  #    sides_menu
-  #  else
-     extras
+  if @sides_input > 2
+    puts "Invalid entry, please select an item."
+    sides_menu
+  else
+    extras
+  end
 end
 
 def first_choice
@@ -72,12 +77,16 @@ def again
 end
 
 def totals
-  #wallet - if total exceeds > what they say they have then...
   total = 0
   calories = 0
   @plates.each do |i|
     total += i[:cost]
   end
+    if total > @wallet
+      puts "You have gone over your budget, order again."
+      @plates.clear
+      main_menu
+    end
   @plates.each do |c|
     calories += c[:calories]
   end
@@ -87,15 +96,14 @@ def totals
 end
 
 def order_new_meal
-  #ask if they want to pay or be done, or order new meal
   puts "\nWould you like to order another meal? (y/n)"
   new_meal = gets.strip.downcase
   if new_meal == 'y'
-    main_menu
+    wallet
   else
     puts "Thank you for your order."
     exit
   end
 end
 
-main_menu
+wallet
